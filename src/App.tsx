a3c53ffd7a6eb7b36f70aeec6ddcb68eb00c34b5
Andy Pickler <andy.pickler@gmail.com>
2023-09-30 08:35:55 -0600

    Get rid of instantsearch.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import 'instantsearch.css/themes/algolia.css';
 import React from 'react';
 import { Body } from './Body';
 import Header from './Header';
